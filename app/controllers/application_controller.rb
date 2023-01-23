cecc203a8aa7a150e3a5b8bacce48108ec2bfb38
nhh2 <nhernand@chicagobooth.edu>
2023-01-23 02:36:55 +0000

    RPS working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,27 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
-# defining the method "play_rock"
-  def play_rock
-    # write a ton of code
-    # redirect_to("https://www.wikipedia.org")
-    # render ({ :html => "<h1>Hello, world!</h1>".html_safe })
-    render ({ :template => "game_templates/user_rock.html.erb"})
-  end
+    # defining the method "homepage"
+      def homepage
+        render ({ :template => "game_templates/rules.html.erb"})
+      end
+
+    # defining the method "play_rock"
+      def play_rock
+        # test code as I was following the video 
+            # redirect_to("https://www.wikipedia.org")
+            # render ({ :html => "<h1>Hello, world!</h1>".html_safe })
+        render ({ :template => "game_templates/user_rock.html.erb"})
+      end
+
+    # defining the method "play_paper"
+      def play_paper
+        render ({ :template => "game_templates/user_paper.html.erb"})
+      end
+
+    # defining the method "play_scissors"
+      def play_scissors
+        render ({ :template => "game_templates/user_scissors.html.erb"})
+      end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  get("/rock",{ :controller => "application", :action => "play_rock" })
+
+  # define the "/" URL route
+    get("/",{ :controller => "application", :action => "homepage" })
+
+  # define the "/rock" URL route
+    get("/rock",{ :controller => "application", :action => "play_rock" })
+
+  # define the "/paper" URL route
+    get("/paper",{ :controller => "application", :action => "play_paper" })
+
+  # define the "/scissors" URL route
+    get("/scissors",{ :controller => "application", :action => "play_scissors" })
+
 end
